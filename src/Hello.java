6ca90fb2df30665124373d77973f76ac89759ede
catcat@example.com <ag9999@ua.fm>
2012-04-11 12:29:54 +0300

    legacy fix

--- src/Hello.java
+++ src/Hello.java
@@ -2,6 +2,7 @@ public class Hello {
     public static void main(String ... arg) {
         System.out.println("Hello world");
         System.out.println("Hello new commit");
+        System.out.println("Legacy code fix");
     }
 
     public void immediateFix() {
